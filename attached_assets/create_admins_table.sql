1ced45a3275f69891828a3781aa06e11c47df4fe
ekalbevoldog <b@Blakegodlove.com>
2025-04-28 11:41:06 -0500

    Delete attached_assets directory

--- attached_assets/create_admins_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE admins (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  created_at TIMESTAMP DEFAULT NOW(),
-  name TEXT,
-  email TEXT,
-  auth_user_id UUID REFERENCES auth.users(id)
-);
--- attached_assets/create_compliance_officers_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE compliance_officers (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  created_at TIMESTAMP DEFAULT NOW(),
-  name TEXT,
-  email TEXT,
-  institution TEXT,
-  auth_user_id UUID REFERENCES auth.users(id)
-);
